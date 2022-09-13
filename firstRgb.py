0eb63284ab046238e3e3fc28373a7d1e2fd78d9d
mzimmer77 <mzimmer77@charlottesvilleschools.org>
2022-09-13 09:46:16 -0400

    finished blinkeer

--- firstRgb.py
+++ firstRgb.py
@@ -1,10 +1,17 @@
+
 import board
 import neopixel
-
+import time
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-dot.brightness = 0.1
+dot.brightness = 0.5 
 
 print("Make it red!")
 
 while True:
     dot.fill((255, 0, 0))
+    print("Red")
+    time.sleep(0.5)
+
+    dot.fill((255, 0, 255))
+    print("Purple")
+    time.sleep(0.5)
